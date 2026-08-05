6306423a48baad6aa1a36a5cfc7e8fc7773c463e
DaveChessington <davidadavaz@outlook.com>
2026-08-04 21:14:57 -0600

    feat: configure IAM service infrastructure with database settings

--- iam/config.py
+++ iam/config.py
@@ -11,8 +11,8 @@ class Config:
     else:
         user = os.environ.get('DB_USER') or 'postgres'
         password = os.environ.get('DB_PASSWORD') or 'root'
-        host = os.environ.get('HOST') or '127.0.0.1'
-        port = os.environ.get('PORT') or '5432'
+        host = os.environ.get('DB_HOST') or os.environ.get('HOST') or '127.0.0.1'
+        port = os.environ.get('DB_PORT') or os.environ.get('PORT') or '5432'
         db_name = os.environ.get('DB_NAME') or 'gestion_horarios_udl'
         SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg://{user}:{password}@{host}:{port}/{db_name}"
 
